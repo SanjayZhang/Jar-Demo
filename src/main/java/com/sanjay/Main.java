999c46f0a54b2a8fe82d202bed5ff8ca29d9d869
Sanjay <sanjay.zsj09@gmail.com>
2016-08-07 18:15:52 +0800

    init jar demo

---
+++ src/main/java/com/sanjay/Main.java
@@ -0,0 +1,10 @@
+package com.sanjay;
+
+/**
+ * Created by zhangsangjie@myhexin.com on 2016/8/7.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("jar");
+    }
+}
